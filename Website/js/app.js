6c43b8d9390f88215cd82411b2fd80d4c0d2cff8
William Wenzel <wrw3355@rit.edu>
2014-04-23 07:39:15 -0400

    Added static view mockups.

--- Website/js/app.js
+++ Website/js/app.js
@@ -2,6 +2,15 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
     this.resource("index", { path: "/" });
+    this.resource("location", function() {
+        this.route("new");
+    });
+    this.resource("restaurant", function() {
+        this.route("new");
+    });
+    this.resource("event", function() {
+        this.route("new");
+    });
 });
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
